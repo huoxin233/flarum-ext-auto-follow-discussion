94faec23e615b8ff377212f3dfc6dc5bae40f236
huoxin <23447157+huoxin233@users.noreply.github.com>
2024-06-15 20:26:38 +0800

    style: fix formatting with prettier

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,17 +1,18 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('huoxin/auto-follow-discussion', () => {
-  app.extensionData.for('huoxin-auto-follow-discussion').registerSetting({
-    setting: 'huoxin-auto-follow-discussion.defaultFollowAfterCreate',
-    type: 'switch',
-    label: app.translator.trans('huoxin-auto-follow-discussion.admin.default-follow-after-create-label'),
-    help: app.translator.trans('huoxin-auto-follow-discussion.admin.default-follow-after-create-help'),
-  })
-  .registerSetting({
-    setting: 'huoxin-auto-follow-discussion.defaultFollowAfterRead',
-    type: 'switch',
-    label: app.translator.trans('huoxin-auto-follow-discussion.admin.default-follow-after-read-label'),
-    help: app.translator.trans('huoxin-auto-follow-discussion.admin.default-follow-after-read-help'),
-  });
-  
+  app.extensionData
+    .for('huoxin-auto-follow-discussion')
+    .registerSetting({
+      setting: 'huoxin-auto-follow-discussion.defaultFollowAfterCreate',
+      type: 'switch',
+      label: app.translator.trans('huoxin-auto-follow-discussion.admin.default-follow-after-create-label'),
+      help: app.translator.trans('huoxin-auto-follow-discussion.admin.default-follow-after-create-help'),
+    })
+    .registerSetting({
+      setting: 'huoxin-auto-follow-discussion.defaultFollowAfterRead',
+      type: 'switch',
+      label: app.translator.trans('huoxin-auto-follow-discussion.admin.default-follow-after-read-label'),
+      help: app.translator.trans('huoxin-auto-follow-discussion.admin.default-follow-after-read-help'),
+    });
 });
